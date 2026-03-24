6598feb3a6f2effa1d09579fae485f6b6bbce817
Nova Brown <novabrown2007@gmail.com>
2026-03-24 12:02:42 -0400

    Delete core/interface/desktopInterface directory

--- core/interface/desktopInterface/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Desktop interface package for Aura.
-
-This package contains desktop-specific interface implementations and
-bootstrap helpers. Platform variants (such as Windows) live in
-subpackages so each target can evolve independently without affecting
-the core runtime modules.
-"""
-
